ad1ed64cc0bf799b9b8b5652edc89bd0f40cdffc
unknown <bhandari.krish.kb@gmail.com>
2026-04-12 00:55:37 +0530

    Added Projects Controller with basic CRUD

---
+++ UttuHub/UttuHub.API/Controllers/ProjectsController.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UttuHub.API.Data;
+using UttuHub.API.Models;
+
+namespace UttuHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProjectsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // UC 231 - Create Project
+        [HttpPost]
+        public async Task<ActionResult<Project>> PostProject(Project project)
+        {
+            _context.Projects.Add(project);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetProjects), new { id = project.Id }, project);
+        }
+
+        // UC 232 - GET all Projects
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Project>>> GetProjects()
+        {
+            return await _context.Projects.ToListAsync();
+        }
+
+        // UC 233 - Update Project
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProject(int id, Project project)
+        {
+            if (id != project.Id)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            _context.Entry(project).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!_context.Projects.Any(e => e.Id == id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // UC 234 - Delete Project
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            var project = await _context.Projects.FindAsync(id);
+            if (project == null)
+            {
+                return NotFound();
+            }
+
+            _context.Projects.Remove(project);
+            await _context.SaveChangesAsync();
+
+            return NoContent(); 
+        }
+    }
+}
